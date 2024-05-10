5fbad20bbb080bd8d9c541f145d492ac9e26d608
karulation <125230705+karulation@users.noreply.github.com>
2024-05-10 14:57:41 +0800

    add intro mio

--- moreCMD.js
+++ moreCMD.js
@@ -333,6 +333,17 @@ function additionalCommmand(isCommand) {
                 );
                 break;
 
+            case 'mio':
+                var media = 'https://telegra.ph/file/f9285102bf2fb0acfa71c.jpg';
+                var textMessage = "‿︵‿︵୨˚̣̣̣͙୧ - - ୨˚̣̣̣͙୧‿︵‿︵\nNEO STAFF INFO\nName : Mio\nPosition : Administrator and Head of Discord Department\n\nNot kind, not bad either. Quite knowledgeable but will always have the heart open to learn something new, I hope so. Dangerous but silent at times. Into cars and florist, a long journey until I will be able to live such a life. Enjoy reading Manga & LN. Plays VN, Racing Simulator & FPS, wish to improve on such a short duration.";
+                XliconBotInc.sendMessage(
+                    from, {
+                      image: media,
+                      caption: textMessage,
+                    }, { quoted: m }
+                );
+                break;
+
             
             // -------------------------------- NEO INTRO END ------------------------------------
             
